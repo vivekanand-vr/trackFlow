d24c37105d6bb248b05232bbaf925ca396c3b374
vivekanand-vr <bendragon21@gmail.com>
2024-12-30 23:29:47 +0530

    Refactor: Remove authentication, add hero note, and implement popup warning

--- trackFlow/src/App.jsx
+++ trackFlow/src/App.jsx
@@ -1,29 +1,11 @@
-import React, { useState, useContext } from 'react';
-import { UserContext } from './context/userContext';
-import SignIn from './components/SignIn';
-import LandingPage from './pages/LandingPage';
-import Dashboard from './pages/Dashboard';
+import React from 'react';
+import Dashboard from './Dashboard';
 
 function App() {
-  const { user } = useContext(UserContext);
-  const [showModal, setShowModal] = useState(false);
-  
-  const toggleModal = () => {
-    setShowModal(!showModal);
-  };
-  
-  if (user) {
-    return (<body className='min-h-screen bg-gradient-to-r from-[#3E0000] to-[#7C0000]'>
-              <Dashboard />
-            </body>
-          );
-  }
-
   return (
-    <>
-      <LandingPage onClick={toggleModal} />
-      <SignIn isOpen={showModal} toggleModal={toggleModal} />
-    </>
+    <div className='min-h-screen bg-gradient-to-r from-[#3E0000] to-[#7C0000]'>
+      <Dashboard />
+    </div>
   );
 }
 
--- trackFlow/src/main.jsx
+++ trackFlow/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { UserProvider } from './context/userContext.jsx';
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <App />
   </StrictMode>,
 )
